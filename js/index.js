f389c6f771423902ed0a7e797bc17131d9e9a777
mmihelca <mmihelca@gmail.com>
2023-12-16 22:05:35 +0100

    final touches

--- js/index.js
+++ js/index.js
@@ -3,19 +3,45 @@ function showLocalTime(event) {
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
-  let cityElement = document.querySelector(".city");
+  let cityElement = document.querySelector(".first-cities");
   let displayName = cityTimeZone.replace("_", " ").split("/")[1];
   cityElement.innerHTML = `
-    <div class="left">
-    <h2>${displayName}</h2>
-    <p class="date">${moment().tz(cityTimeZone).format("MMMM Do, YYYY")}</p>
-        </div>
-        <p class="time">${moment()
-          .tz(cityTimeZone)
-          .format("h:mm:ss [<small>]A[</small>]")}</p>`;
+    <div class="city">
+        <div class="left">
+        <h2>${displayName}</h2>
+        <p class="date">${moment().tz(cityTimeZone).format("MMMM Do, YYYY")}</p>
+            </div>
+            <p class="time">${moment()
+              .tz(cityTimeZone)
+              .format("h:mm:ss [<small>]A[</small>]")}</p></div>`;
 }
+setInterval(showLocalTime, 1000);
 
 let citySelectElement = document.querySelector("#cities");
 citySelectElement.addEventListener("change", showLocalTime);
 
-showLocalTime({ target: { value: "current" } });
+setInterval(function () {
+  //Los angeles
+  let losAngelesTime = document.querySelector("#los-angeles .time");
+  let losAngelesDate = document.querySelector("#los-angeles .date");
+  losAngelesDate.innerHTML = moment().format("MMMM Do, YYYY");
+  losAngelesTime.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss [<small>]A[</small>]");
+
+  //Paris
+  let parisTime = document.querySelector("#paris .time");
+  let parisDate = document.querySelector("#paris .date");
+  parisDate.innerHTML = moment().format("MMMM Do, YYYY");
+  parisTime.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("h:mm:ss [<small>]A[</small>]");
+
+  // Buenos Aires
+  let buenosAiresTime = document.querySelector("#buenos-aires .time");
+  let buenosAiresDate = document.querySelector("#buenos-aires .date");
+  buenosAiresDate.innerHTML = moment().format("MMMM Do, YYYY");
+  buenosAiresTime.innerHTML = moment()
+    .tz("America/Argentina/Buenos_Aires")
+    .format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
