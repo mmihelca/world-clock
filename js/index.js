42bd6bf9f076cd3a4212dd2799751ea4efd83024
mmihelca <mmihelca@gmail.com>
2023-12-16 20:02:40 +0100

    added moment js, timezones and basic locations

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log("HELLO WORLD");
+function updateTime() {
+  //Los angeles
+  let losAngelesTime = document.querySelector("#los-angeles .time");
+  let losAngelesDate = document.querySelector("#los-angeles .date");
+  losAngelesDate.innerHTML = moment().format("MMMM Do, YYYY");
+  losAngelesTime.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss A");
+
+  //Sydney
+  let sydneyTime = document.querySelector("#sydney .time");
+  let sydneyDate = document.querySelector("#sydney .date");
+  sydneyDate.innerHTML = moment().format("MMMM Do, YYYY");
+  sydneyTime.innerHTML = moment().tz("Australia/Sydney").format("h:mm:ss A");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
