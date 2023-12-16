ee51ca88e0cf76e81befff952b337202bd18f2ad
mmihelca <mmihelca@gmail.com>
2023-12-16 20:57:41 +0100

    added js functionality to the selection menu

--- js/index.js
+++ js/index.js
@@ -1,22 +1,16 @@
-function updateTime() {
-  //Los angeles
-  let losAngelesTime = document.querySelector("#los-angeles .time");
-  let losAngelesDate = document.querySelector("#los-angeles .date");
-  losAngelesDate.innerHTML = moment()
-    .tz("America/Los_Angeles")
-    .format("MMMM Do, YYYY");
-  losAngelesTime.innerHTML = moment()
-    .tz("America/Los_Angeles")
-    .format("h:mm:ss A");
-
-  //Sydney
-  let sydneyTime = document.querySelector("#sydney .time");
-  let sydneyDate = document.querySelector("#sydney .date");
-  sydneyDate.innerHTML = moment()
-    .tz("Australia/Sydney")
-    .format("MMMM Do, YYYY");
-  sydneyTime.innerHTML = moment().tz("Australia/Sydney").format("h:mm:ss A");
+function showLocalTime(event) {
+  let cityElement = document.querySelector(".city");
+  let cityTimeZone = event.target.value;
+  let displayName = cityTimeZone.replace("_", " ").split("/")[1];
+  cityElement.innerHTML = `
+    <div class="left">
+    <h2>${displayName}</h2>
+    <p class="date">${moment().tz(cityTimeZone).format("MMMM Do, YYYY")}</p>
+        </div>
+        <p class="time">${moment().tz(cityTimeZone).format("h:mm:ss A")}</p>`;
 }
 
-updateTime();
-setInterval(updateTime, 1000);
+let citySelectElement = document.querySelector("#cities");
+citySelectElement.addEventListener("change", showLocalTime);
+
+showLocalTime({ target: { value: "Europe/London" } });
